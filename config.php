e66c3b980f88090e2bdd48a7664e20e7b0d44fe6
Prophet731 <Prophet731@users.noreply.github.com>
2021-04-27 10:58:50 -0700

    Fix dotenv path

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
     require __DIR__ . '/../../../vendor/autoload.php';
 
-    $dotenv = new \Dotenv\Dotenv(__DIR__ . '/../../../../');
+    $dotenv = new \Dotenv\Dotenv(__DIR__ . '/../../../');
     $dotenv->load();
 } else {
     die('Failed to load');
